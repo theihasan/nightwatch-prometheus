e87c791841703bd10a2f01d4a09e074e8afaff53
Abul Hassan <imabulhasan99@gmail.com>
2026-08-02 16:43:44 +0600

    bismillah. initial setup for this package

---
+++ README.md
@@ -0,0 +1,5 @@
+# Nightwatch Promethus
+
+Minimal package skeleton for `theihasan/nightwatch-promethus`.
+
+This package will expose Laravel Nightwatch metrics in Prometheus format.
---
+++ src/NightwatchPromethusServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Ihasan\NightwatchPromethus;
+
+use Illuminate\Support\ServiceProvider;
+
+class NightwatchPromethusServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        //
+    }
+
+    public function boot(): void
+    {
+        //
+    }
+}
