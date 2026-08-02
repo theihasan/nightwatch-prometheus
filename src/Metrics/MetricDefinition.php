2b5cfb9b17d13b2cfac930f905df1dc54ab42c78
Abul Hassan <imabulhasan99@gmail.com>
2026-08-03 01:25:26 +0600

    Add HTTP request duration histogram metric
    The new `httpRequestDurationSeconds` metric tracks request latency with
    predefined buckets ranging from 1ms to 10s. The `MetricDefinition` class
    was updated to support an optional `buckets` parameter for histogram metrics.

--- src/Metrics/MetricDefinition.php
+++ src/Metrics/MetricDefinition.php
@@ -6,12 +6,14 @@ class MetricDefinition
 {
     /**
      * @param list<string> $labels
+     * @param list<float> $buckets
      */
     public function __construct(
         public string $name,
         public string $type,
         public string $help,
         public array $labels,
+        public array $buckets = [],
     ) {
         //
     }
--- src/Metrics/MetricDefinitions.php
+++ src/Metrics/MetricDefinitions.php
@@ -24,6 +24,17 @@ class MetricDefinitions
         );
     }
 
+    public function httpRequestDurationSeconds(): MetricDefinition
+    {
+        return new MetricDefinition(
+            'nightwatch_http_request_duration_seconds',
+            'histogram',
+            'HTTP request duration observed by Nightwatch Promethus',
+            ['method', 'route', 'status_code'],
+            [0.001, 0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1.0, 2.5, 5.0, 10.0],
+        );
+    }
+
     /**
      * @return array<string, MetricDefinition>
      */
@@ -32,6 +43,7 @@ class MetricDefinitions
         $definitions = [
             $this->httpRequestsTotal(),
             $this->logsTotal(),
+            $this->httpRequestDurationSeconds(),
         ];
 
         $indexedDefinitions = [];
