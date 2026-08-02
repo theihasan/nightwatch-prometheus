ba36b4d2fc67ab747737ba1857c410352c51fcee
Abul Hassan <imabulhasan99@gmail.com>
2026-08-02 17:36:22 +0600

    Add NightwatchPromethusState debug class

---
+++ src/Debug/NightwatchPromethusState.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Ihasan\NightwatchPromethus\Debug;
+
+use Ihasan\NightwatchPromethus\Contracts\MetricSink;
+use Ihasan\NightwatchPromethus\Ingest\NightwatchPromethusIngest;
+use Ihasan\NightwatchPromethus\Metrics\InMemoryMetricSink;
+
+class NightwatchPromethusState
+{
+    public function __construct(
+        private NightwatchPromethusIngest $ingest,
+        private MetricSink $metricSink,
+    ) {
+        //
+    }
+
+    /**
+     * @return array{
+     *     ingest: array{write_count: int, write_now_count: int, last_record_type: ?string, record_type_counts: array<string, int>},
+     *     metrics: array<string, float>
+     * }
+     */
+    public function snapshot(): array
+    {
+        return [
+            'ingest' => [
+                'write_count' => $this->ingest->writeCount(),
+                'write_now_count' => $this->ingest->writeNowCount(),
+                'last_record_type' => $this->ingest->lastRecordType(),
+                'record_type_counts' => $this->ingest->recordTypeCounts(),
+            ],
+            'metrics' => $this->metricSink instanceof InMemoryMetricSink
+                ? $this->metricSink->counters()
+                : [],
+        ];
+    }
+}
